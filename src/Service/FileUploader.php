3a538d82869691521a1717042829a4b2b4f46c23
Aurélien Litti <litti.aurelien@gmail.com>
2021-05-10 21:34:24 +0200

    add: maximize images uploaded to 2000x2000 by using Imagine

--- src/Service/FileUploader.php
+++ src/Service/FileUploader.php
@@ -5,13 +5,21 @@ namespace App\Service;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
+use Imagine\Gd\Imagine;
+use Imagine\Image\Box;
+
 class FileUploader
 {
+    private const MAX_WIDTH = 2000;
+    private const MAX_HEIGHT = 2000;
+
     private $targetDirectory;
+    private $imagine;
 
     public function __construct($targetDirectory)
     {
-        $this->targetDirectory = $targetDirectory;
+        $this->targetDirectory  = $targetDirectory;
+        $this->imagine          = new Imagine();
     }
 
     public function upload($file, string $additionnalPath = '', string $newFilename = '')
@@ -34,6 +42,10 @@ class FileUploader
 
             try {
                 $file->move($finalPath, $fileName);
+                if (function_exists('mime_content_type') && preg_match('/image/', mime_content_type($finalPath . '/' . $fileName))) {
+                    // Resize new uploaded file
+                    $this->resize($finalPath . '/' . $fileName);
+                }
             } catch (FileException $e) {
                 // ... handle exception if something happens during file upload
                 dump($e);
@@ -52,5 +64,21 @@ class FileUploader
     public function slugify($string)
     {
         return transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $string);
+    }
+
+    public function resize(string $filename): void
+    {
+        list($iwidth, $iheight) = getimagesize($filename);
+        $ratio = $iwidth / $iheight;
+        $width = self::MAX_WIDTH;
+        $height = self::MAX_HEIGHT;
+        if ($width / $height > $ratio) {
+            $width = $height * $ratio;
+        } else {
+            $height = $width / $ratio;
+        }
+
+        $photo = $this->imagine->open($filename);
+        $photo->resize(new Box($width, $height))->save($filename);
     }
 }
